e1d379f34578ee7a5cc2aa1216029a4b6ff19449
Leonardo <leoallvez@hotmail.com>
2021-03-22 23:51:19 -0300

    field injection

--- app/src/main/java/io/github/leoallvez/hilt/MainActivity.kt
+++ app/src/main/java/io/github/leoallvez/hilt/MainActivity.kt
@@ -2,14 +2,21 @@ package io.github.leoallvez.hilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.leoallvez.hilt.database.DatabaseAdapter
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject lateinit var databaseAdapter: DatabaseAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        Log.d(TAG, "DatabaseAdapter: $databaseAdapter")
+        databaseAdapter.log("Hello Hilt")
+    }
 }
